5015b9d52d20919add98a19e113cd275f7be21b2
Leszek Prabucki <leszek.prabucki@gmail.com>
2011-12-06 20:43:14 +0100

    Make possible generate coverage report

--- vendor/vendors.php
+++ vendor/vendors.php
@@ -6,7 +6,9 @@ set_time_limit(0);
 $vendorDir = __DIR__;
 $deps = array(
     array('symfony', 'http://github.com/symfony/symfony', isset($_SERVER['SYMFONY_VERSION']) ? $_SERVER['SYMFONY_VERSION'] : 'origin/master'),
-    array('twig', 'http://github.com/fabpot/Twig', 'origin/master')
+    array('twig', 'http://github.com/fabpot/Twig', 'origin/master'),
+    array('doctrine', 'http://github.com/doctrine/doctrine2.git', 'origin/master'),
+    array('doctrine-common', 'http://github.com/doctrine/common.git', 'origin/master')
 );
 
 foreach ($deps as $dep) {
