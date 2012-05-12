0ec0bbce47b39801f1848643ac6711b0c07fb194
Jakub Zalas <jakub@zalas.pl>
2012-05-12 12:07:13 +0100

    Added fixture project to be used with behat scenarios (so there is no need of installing the bundle in a Symfony project to run scenarios). Added composer.json.

---
+++ Features/Fixtures/Project/app/autoload.php
@@ -0,0 +1,18 @@
+<?php
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+$vendorDir = __DIR__.'/../../../../vendor';
+$loader = require_once $vendorDir.'/.composer/autoload.php';
+
+// intl
+if (!function_exists('intl_get_error_code')) {
+    require_once $vendorDir.'/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
+    $loader->add(null, $vendorDir.'/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs');
+}
+
+AnnotationRegistry::registerLoader(function($class) use ($loader) {
+    $loader->loadClass($class);
+    return class_exists($class, false);
+});
+AnnotationRegistry::registerFile($vendorDir.'/doctrine/orm/lib/Doctrine/ORM/Mapping/Driver/DoctrineAnnotations.php');
